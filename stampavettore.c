cda14431fbaf3b7593b31665b828d4418fc8370b
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-04-24 18:20:47 +0200

    vector exercise

---
+++ stampavettore.c
@@ -0,0 +1,13 @@
+// stampavettore.c
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    
+	int B[5]; 
+
+	B[0]= 4; B[1]= 3; B[2]= 2; B[3]= 1; B[4]= 0;
+
+	// aggiungi il codice che manca (1 ciclo for contenente una printf)
+}

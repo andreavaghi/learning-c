e147a19d3e574f48598b0d50e0e654828ce3d7af
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-04-26 21:31:54 +0200

    array exercise

---
+++ immettiarray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10   
+// usa opportunamente questa costante per impostare il numero dei cicli nel for
+
+void main()
+{
+    
+	int C[N]; 
+	
+	// aggiungi qui il ciclo che chiede con delle scanf gli interi all'utente
+
+
+
+
+	// aggiungi qui il ciclo che stampa gli elementi del vettore con le printf
+}

ba3cd2e9da9faf9d5e798f3ecd2724d6a277671a
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-04-28 13:02:21 +0200

    finish immettiarray

--- immettiarray.c
+++ immettiarray.c
@@ -4,18 +4,23 @@
 #define N 10   
 // usa opportunamente questa costante per impostare il numero dei cicli nel for
 
-void main()
+int main()
 {
     
 	int C[N];
 	int i;
 	
 	// aggiungi qui il ciclo che chiede con delle scanf gli interi all'utente
-        for(i = 0; i < N; i++) {
-        	
-        }
-
-
+    for(i = 0; i < N; i++) {
+    	printf("Inserisci il numero alla posizione %d\n", i);
+    	scanf("%d", &C[i]);
+    }
 
 	// aggiungi qui il ciclo che stampa gli elementi del vettore con le printf
+	for(i = 0; i < N; i++) {
+    	printf("Il numero alla posizione %d e' %d\n", i, C[i]);
+    }
+
+    getchar();
+	return 0 ;
 }

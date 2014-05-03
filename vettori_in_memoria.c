7f249aacda4ec3ebb996197c2fb2cbb9e92a4283
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-05-03 17:17:22 +0200

    vector exercise 2

---
+++ vettori_in_memoria.c
@@ -0,0 +1,15 @@
+//vettori_in_memoria.c
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5 
+// usa opportunamente questa costante per impostare il numero dei cicli nel for
+
+int main()
+{
+    int B[N]; 
+    B[0]= 4; B[1]= 3; B[2]= 2; B[3]= 2; B[4]= 1;
+    
+
+   // inserire il codice mancante
+}

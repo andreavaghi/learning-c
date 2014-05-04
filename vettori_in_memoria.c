3f282f517c32605048f651706cd5b35e8528e27e
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-05-04 18:39:08 +0200

    vettori in memoria

--- vettori_in_memoria.c
+++ vettori_in_memoria.c
@@ -8,7 +8,12 @@
 int main()
 {
     int B[N]; 
+    int i;
     B[0]= 4; B[1]= 3; B[2]= 2; B[3]= 2; B[4]= 1;
+
+    for(i = 0; i < N; i++) {
+    	printf("%d\n", B[i]);
+    }
     
 
    // inserire il codice mancante
